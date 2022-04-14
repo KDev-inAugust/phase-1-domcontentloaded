1ce3b6ed04f93c51652c1d742ad84cf30a466856
Kane Mathis <admin@kanemathis.com>
2022-04-14 17:26:19 -0500

    commit tree debb2f3518130c0cbdbf63c6394e7985abf2aeb9

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function (){
+    const index = document.getElementById("text");
+    index.textContent= "This is really cool!"
+})
+
